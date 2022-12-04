de849512d3d9756355434adf4efbed15b115f1b3
Samar singh <119495664+samarsingh10@users.noreply.github.com>
2022-12-04 12:42:09 +0530

    Update dict.py

--- dict.py
+++ dict.py
@@ -1,11 +1,11 @@
-'''a1=list(map(input("keys: ").split(",")))
+a1=list(map(input("keys: ").split(",")))
 a2=list(map(input("values: ").split(",")))
 print(dict(zip(a,a2)))
 l=a2
 for i in a2:
     for j in range(0,len(a2)):
         l.pop(1[j])
-        w=w-1'''
+        w=w-1
 dict1={1,2,3,4,5,6,6}
 d=frozenset(dict1)
 print(d)
